906ac374c8430cc3c83256cd408c356c38f5dbf7
archdoor <archdoor@outlook.com>
2017-05-02 21:38:10 +0800

    config files update

--- config.sh
+++ config.sh
@@ -2,31 +2,34 @@
 
 CUR_DIR=$(pwd)
 
-echo "config the plugin vundle..."
+echo "设置 vundle..."
 if [ ! -d  ${CUR_DIR}/vim/bundle/vundle ]; then
 	git clone https://github.com/gmarik/vundle.git ${CUR_DIR}/vim/bundle/vundle
 else
-	echo -e "\tPlugin vundle is already exist!"
+	echo -e "\t插件 vundle 已经存在！"
 fi
 
-echo "config the softlink .vimrc..."
+echo "设置 .vimrc..."
 ln -sfT ${CUR_DIR}/vimrc ~/.vimrc
 
-echo "config the softlink .gvimrc..."
+echo "设置 .gvimrc..."
 ln -sfT ${CUR_DIR}/gvimrc ~/.gvimrc
 
-echo "config the softlink .vim..."
+echo "设置 .vim..."
 ln -sfT ${CUR_DIR}/vim ~/.vim
 
-echo "config the xfce4-terminal..."
-cp -f ${CUR_DIR}/vim/terminal/terminalrc ~/.config/xfce4/terminal/
+echo "设置 xfce4-terminal..."
+cp -f ${CUR_DIR}/vim/xfce4-terminal/terminalrc ~/.config/xfce4/terminal/
 
-echo "config the xfce4-terminal dir-colors..."
+echo "设置 xfce4-terminal dir-colors..."
 if [ -d  ${CUR_DIR}/vim/bundle/dircolors-solarized ]; then
-	eval `dircolors ${CUR_DIR}/vim/bundle/dircolors-solarized/dircolors.256dark`
+	echo -e "\
+	\n# [DIR_COLORS]\
+	\neval \`dircolors ~/.vim/bundle/dircolors-solarized/dircolors.256dark\`\
+	\n" >> ~/.bashrc
 else
 	echo -e "\tPlugin dir-colors is not exist!"
 fi
 
-echo "all configs done!"
+echo "设置完成！"
 
