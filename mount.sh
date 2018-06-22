80720fb8300f490e5d6d59a18e14337861f834a1
archdoor <archdoor@outlook.com>
2018-06-22 18:20:52 +0800

    mount文件修改

--- mount.sh
+++ mount.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo mount -t cifs -o soft,username=jett,password=liujuntao,uid=1000,gid=100 //172.16.0.158/liujuntao ~/windows
+sudo mount -t cifs -o soft,username=jett,password=liujuntao,uid=1000,gid=100,vers=2.0 //172.16.0.158/liujuntao ~/windows
